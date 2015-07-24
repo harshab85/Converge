4f089e924ff696e2ff92578dbe200a4f024e2489
harshab85 <harshab85@gmail.com>
2015-07-23 22:49:18 -0400

    The first commit for this project with the skeleton files

---
+++ converge/converge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  converge
+//
+//  Created by Harsha Bala on 2015-07-23.
+//  Copyright (c) 2015 Converge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
