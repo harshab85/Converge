1158a5f5c8c1d8d44eacc01cb1946a3e80851ace
harshab85 <harshab85@gmail.com>
2015-07-23 23:53:46 -0400

    Created planner view with address text box, time picker and cancel button

---
+++ converge/converge/PlannerViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PlannerViewController.swift
+//  converge
+//
+//  Created by Harsha Bala on 2015-07-23.
+//  Copyright (c) 2015 Converge. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class PlannerViewController: UIViewController{
+    
+}
